7007ddb54bed78db7c9363aacd6617ca3cb05028
jaredpek <83465527+jaredpek@users.noreply.github.com>
2022-05-14 22:49:08 +0800

    load levels locked and unlocked function
    load levels locked and unlocked function

--- Jumping Game/Assets/Resources/Scripts/LoadLevels/LoadLevels.cs
+++ Jumping Game/Assets/Resources/Scripts/LoadLevels/LoadLevels.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,6 +15,36 @@ public class LoadLevels : MonoBehaviour
             GameObject newButton = Instantiate(levelButton, levelHolder.transform) as GameObject;
             newButton.name = "Level " + (i+1);
             newButton.GetComponentInChildren<TextMeshProUGUI>().SetText("Level " + (i+1));
+        }
+    }
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadLevels : MonoBehaviour
+{
+    public GameObject levelHolder;
+    public GameObject levelButton;
+    public int levelQty = 20;
+    public int levelUnlocked = 1;
+
+    private void Start(){
+        for(int i=1; i<=levelQty; i++){
+            GameObject newButton = Instantiate(levelButton, levelHolder.transform) as GameObject;
+            if(i <= levelUnlocked){
+                newButton.name = "Level " + i;
+                newButton.GetComponentInChildren<TextMeshProUGUI>().SetText("Level " + i);
+                newButton.GetComponent<Button>().interactable = true;
+                Debug.Log("true" + i);
+            }else{
+                newButton.name = "Level " + i + " Locked";
+                newButton.GetComponentInChildren<TextMeshProUGUI>().SetText("Level " + i + "\nLocked");
+                newButton.GetComponent<Button>().interactable = false;
+                Debug.Log("false" + i);
+            }
         }
     }
 }
