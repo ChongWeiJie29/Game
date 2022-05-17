a22e618a877e5a4c45f606ac0d1afc94da6ff858
WeiJie2909 <chongweijie29@gmail.com>
2022-05-17 09:55:38 +0800

    V1
    Separared Level1 from main menu

--- Jumping Game/Assets/Resources/Scripts/Levels/Level1.cs
+++ Jumping Game/Assets/Resources/Scripts/Levels/Level1.cs
@@ -5,12 +5,13 @@ using System.Linq;
 
 public class Level1 : MonoBehaviour
 {
+    [SerializeField]   
     private GameObject selectedCharacter;
     private GameObject pauseScreen;
     // Start is called before the first frame update
     void Start()
     {
-        selectedCharacter = CharacterContainer.unlockedCharacters.ElementAt(CharacterContainer.selectedCharacter);
+        // selectedCharacter = CharacterContainer.unlockedCharacters.ElementAt(CharacterContainer.selectedCharacter);
         selectedCharacter = Instantiate(selectedCharacter, new Vector3(0, -3, 0), Quaternion.identity);
         selectedCharacter.transform.rotation = Quaternion.Euler(0,0,-90);
         selectedCharacter.AddComponent<PlayerControls>();
