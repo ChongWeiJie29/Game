76f85305e5a4c949d43e20f869a25e8f2cfda1c7
jaredpek <83465527+jaredpek@users.noreply.github.com>
2022-05-18 21:07:55 +0800

    platforms
    platforms

--- Jumping Game/Assets/Resources/Scripts/Levels/Platforms/spiky_thick.cs
+++ Jumping Game/Assets/Resources/Scripts/Levels/Platforms/spiky_thick.cs
@@ -7,13 +7,11 @@ public class spiky_thick : MonoBehaviour
     private BoxCollider2D playerCollider;
     [SerializeField]
     private BoxCollider2D platformCollider;
-    [SerializeField]
-    private BoxCollider2D platformTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
-        Physics2D.IgnoreCollision(platformCollider, platformTrigger, true);
+        
     }
     // Called once per frame
     void Update()
@@ -21,16 +19,6 @@ public class spiky_thick : MonoBehaviour
         playerCollider = Level1.selectedCharacterCollider.GetComponent<BoxCollider2D>();
         if(platformCollider.IsTouching(playerCollider)){
             PlayerControls.jumping = false;
-        }
-    }
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.name==playerCollider.gameObject.name){
-            Physics2D.IgnoreCollision(platformCollider, playerCollider, true);
-        }
-    }
-    void OnTriggerExit2D(Collider2D other){
-        if(other.gameObject.name==playerCollider.gameObject.name){
-            Physics2D.IgnoreCollision(platformCollider, playerCollider, false);
         }
     }
 }
