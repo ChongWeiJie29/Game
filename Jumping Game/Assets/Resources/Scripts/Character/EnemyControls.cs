6498546a2400867e25a1704f953e545bc33dc399
WeiJie2909 <chongweijie29@gmail.com>
2022-05-22 11:08:26 +0800

    V1
    Added enemy controls V1

--- Jumping Game/Assets/Resources/Scripts/Character/EnemyControls.cs
+++ Jumping Game/Assets/Resources/Scripts/Character/EnemyControls.cs
@@ -1,18 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Pathfinding")]
+    public Transform target;
+    public float activateDistance = 1000;
+    public float pathUpdateSeconds = 0.5f;
+
+    [Header("Physics")]
+    public float nextWaypointDistance = 3f;
+    public float jumpNodeHeightRequirement = 0.9f;
+    public float jumpModifier = 300;
+    public float jumpCheckOffset = 0.1f;
+
+    [Header("Custom Behaviour")]
+    public bool followEnabled = true;
+    public bool jumpEnabled = true;
+    public bool directionLookEnabled = true;
+
+    private Path path;
+    private int currentWaypoint = 0;
+    bool isGrounded = false;
+    Seeker seeker;
+    Rigidbody2D enemyRB;
+
     void Start()
     {
-        
+        seeker = GetComponent<Seeker>();
+        enemyRB = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
+    }
+    void FixedUpdate()
+    {
+        target = GameObject.Find("Scientist(Clone)").transform;
+        if (TargetInDistance() && followEnabled)
+        {
+            PathFollow();
+        }
+    }
+    void UpdatePath()
+    {
+        if (followEnabled && TargetInDistance() && seeker.IsDone())
+        {
+            seeker.StartPath(enemyRB.position, target.position, OnPathComplete);
+        }
+    }
+    void PathFollow()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            return;
+        }
+
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - enemyRB.position).normalized;
+        Debug.Log(direction);
+        if (jumpEnabled && isGrounded)
+        {
+            if (direction.y > jumpNodeHeightRequirement)
+            {
+                enemyRB.AddForce(Vector2.up * jumpModifier);
+
+            }
+        }
+
+        enemyRB.AddForce(direction * 30);
+
+        float distance = Vector2.Distance(enemyRB.position, path.vectorPath[currentWaypoint]);
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    bool TargetInDistance()
     {
-        
+        return Vector2.Distance(transform.position, target.transform.position) < activateDistance;
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
     }
 }
